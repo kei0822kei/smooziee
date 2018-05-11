94c7183c5f5112ba9540463d0e1fccde780420d2
mizokami-home <mizokami.keiyu@gmail.com>
2018-05-11 12:35:05 +0900

    alpha

---
+++ scripts/smooziee-fit.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+# deals with phonon scattering experimental data
+###############################################################################
+
+### import modules
+import os
+import sys
+import argparse
+from matplotlib import pyplot as plt
+from smooziee.module import phonon_scattering as ph_scat
+
+### Arg-parser
+parser = argparse.ArgumentParser(
+    description="")
+parser.add_argument('--filename', type=str,
+                    help="input file name  ex) KCl_GXL511_m0p25_RT_4")
+parser.add_argument('--run_mode', type=str,
+                    help="raw or ")
+parser.add_argument('--show', type=bool, default=False,
+                    help="show figure (default) False")
+parser.add_argument('--savefig', type=str,
+                    help="directory where figure is saved")
+args = parser.parse_args()
+
+### main
+fig = plt.figure()
+ax = fig.add_subplot(111)
+scat = ph_scat.Process(args.filename)
+
+if args.run_mode == 'raw':
+    scat.meV_y_unitpk(ax, run_mode=args.run_mode)
+
+if args.run_mode == 'peak':
+    scat.meV_y_unitpk(ax, run_mode=args.run_mode, order=args.order)
+
+if args.run_mode == 'smooth':
+    A_lst = list(map(float, args.param_A.split()))
+    x0_lst = list(map(float, args.param_x0.split()))
+    d_lst = list(map(float, args.param_d.split()))
+    A_lst[0] = int(A_lst[0])
+    x0_lst[0] = int(x0_lst[0])
+    d_lst[0] = int(d_lst[0])
+
+    ### check
+    if len(A_lst) != 2 or len(x0_lst) != 2 or len(d_lst) != 2:
+        print("the number of params is not 2")
+        print("A_lst = %s" % A_lst)
+        print("x0_lst = %s" % x0_lst)
+        print("d_lst = %s" % d_lst)
+        sys.exit(1)
+
+    param_nw_dic = {'A': A_lst, 'x0': x0_lst, 'd': d_lst}
+    scat.meV_y_unitpk(ax, param_nw_dic=param_nw_dic, run_mode=args.run_mode,
+                      order=args.order, threshold=args.threshold)
+
+plt.savefig(args.savefig)
+
+if args.show == True:
+    plt.show()
